0917f5b1107d0fc37fc0eddba71b5df0adbeb9fd
nikilchandrasekar <Nikil.Chandrasekar@indusfusion.com>
2022-07-30 11:25:39 +0530

    storks hello world

--- src/App.js
+++ src/App.js
@@ -35,13 +35,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+        <div>
+          <img src ="storksjob.jpeg"></img>
+        </div>
       </div>
     )
   }
